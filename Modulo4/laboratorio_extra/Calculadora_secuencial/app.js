4b09160097a57de3f5106343dcb1b016d4f89fbb
javsan1985 <javier.santos.merino@gmail.com>
2021-12-10 10:31:40 +0100

    add project

--- Modulo4/laboratorio_extra/Calculadora_secuencial/app.js
+++ Modulo4/laboratorio_extra/Calculadora_secuencial/app.js
@@ -1,37 +1,39 @@
 // Capturar valor input
-var operator = () => parseInt(document.getElementById("input-number").value);
-
-//Guardamos valor de input en partial y lo reseteamos
+var operator = () => (document.getElementById("input-number").value);
 
 function sum() {
-    document.getElementById("result").innerText = operator() + " + ";
+    document.getElementById("result").innerText = partial() + "+";
     document.getElementById("input-number").value = "";
 }
 
 function sub() {
-    document.getElementById("result").innerText = operator() + " - ";
+    document.getElementById("result").innerText = partial() + "-";
     document.getElementById("input-number").value = "";
 }
 
 function mul() {
-    document.getElementById("result").innerText = operator() + " x ";
+    document.getElementById("result").innerText = partial() + "*";
     document.getElementById("input-number").value = "";
 }
 
 function div() {
-    document.getElementById("result").innerText = operator() + " / ";
+    document.getElementById("result").innerText = partial() + "/";
     document.getElementById("input-number").value = "";
 }
 
-
 // Results
-var result = () => document.getElementById("result").innerText = operator();
+var partial = () => document.getElementById("result").innerText + operator(); 
+    document.getElementById("result").innerText = partial(); 
+
+var result = () => document.getElementById("result").innerText = eval(partial()); 
 
 // Events
-document.getElementById("button-result").addEventListener("click", result);
 document.getElementById("button-sum").addEventListener("click", sum);
 document.getElementById("button-subtraction").addEventListener("click", sub);
 document.getElementById("button-multiplication").addEventListener("click", mul);
 document.getElementById("button-division").addEventListener("click", div);
+document.getElementById("button-result").addEventListener("click", result);
+
+
 
 
